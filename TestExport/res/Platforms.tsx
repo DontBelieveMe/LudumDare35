0409ecc1c19c33a7af11aa2448e60059cf241700
Barney Wilks <barneywilks1@gmail.com>
2016-04-17 20:38:04 +0100

    20:38:04.58: (17/04/2016)

---
+++ TestExport/res/Platforms.tsx
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Platforms" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="Platforms.png" width="256" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="isSolid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="isSolid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="isSolid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="isSolid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="isSolid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collidesWith" value="bird"/>
+   <property name="isSolid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collidesWith" value="bird"/>
+   <property name="isSolid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="isSolid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="collidesWith" value="human"/>
+   <property name="harmful" value="all"/>
+   <property name="isSolid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="isSolid" value="false"/>
+   <property name="isWarp" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="isSolid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="isSolid" value="false"/>
+   <property name="isWarp" value="true"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="isSolid" value="false"/>
+   <property name="isWarp" value="true"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="isSolid" value="true"/>
+  </properties>
+ </tile>
+</tileset>
